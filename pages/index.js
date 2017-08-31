680232b05941761d86dbf5cf82837edbcdd7f602
Ondřej Tůma <ondrej@footshop.cz>
2017-08-31 03:27:33 +0200

    og title fix

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ class Index extends Component {
 			<div>
 				<Head>
 					<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-					<meta property="og:title" content={heads.title} />
+					<meta property="og:title" content={`Raf Simons sneakers`} />
 					<meta property="og:type" content="website" />
 					<meta property="og:url" content={heads.url} />
 					<meta property="og:image" content={heads.img} />
