6b988b52e20a2240ab022e7de1a64baff3caedce
Ondřej Tůma <ondrej@footshop.cz>
2017-09-02 04:40:21 +0200

    build

--- out/_next/56300025-154d-4d6b-b33f-5c165d588381/page/index.js
+++ out/_next/56300025-154d-4d6b-b33f-5c165d588381/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[541]);
+            var comp = module.exports=webpackJsonp([2],[],[542]);
             return { page: comp.default }
           })
         
